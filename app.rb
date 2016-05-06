345b9d970c363f80feeeb4ab128027b16e21e792
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-06 13:46:45 +0100

    sometest

--- app.rb
+++ app.rb
@@ -7,7 +7,11 @@ require_relative 'data_mapper_setup'
 class Bookmark < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
-
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
   get '/' do
     redirect '/links'
   end
@@ -20,12 +24,8 @@ class Bookmark < Sinatra::Base
     session[:user_id] = user.id
     redirect to '/links'
   end
-  
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
+
+
 
   get '/links' do
     @links = Link.all
